81c62422cac67ea41cd287177755a1ff5333dda5
HaoHV2k5 <magicmath2k5@gmail.com>
2025-04-11 14:22:18 +0700

    Lesson_Variable
    Lesson_Variable

---
+++ work_space/index.js
@@ -0,0 +1,21 @@
+
+// console.log(`your age is: ${age} year old`);
+// console.log(`the price is: ${price}`);
+// console.log(`your gpa is: ${gpa}`);
+// console.log(typeof age)
+// console.log(typeof price)
+// console.log(typeof gpa)
+
+// let fullName = "huynh van hao";
+// let favariteFood = 'mam kho'
+// console.log(typeof fullName)
+// console.log(`your nam is ${fullName}`);
+// console.log(`my favarite food is ${favariteFood}`)
+
+let fullName = "huynh van hao"
+let age = 20
+let isStudent = true;
+
+document.getElementById("p1").textContent = `your name is : ${fullName}`;
+document.getElementById("p2").textContent = `your age is: ${age}`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`
